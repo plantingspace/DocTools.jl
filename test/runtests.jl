710b960d3e846a3d967d7cb85cd7ab52af9ee52c
theogf <theo.galyfajou@gmail.com>
2023-05-22 13:08:16 +0200

    Fix tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,26 +1,34 @@
 using DocTools
 using Test
 
+const DOCS_DIR = abspath(joinpath(@__DIR__, "..", "docs"))
+const BUILD_DIR = abspath(joinpath(DOCS_DIR, "build"))
+const NOTEBOOKS_DIR = abspath(joinpath(BUILD_DIR, "notebooks"))
+const SRC_ASSETS_DIR = abspath(joinpath(DOCS_DIR, "src", "assets"))
+const SRC_NOTEBOOKS_DIR = abspath(joinpath(DOCS_DIR, "src", "notebooks"))
+
 function build_docs_with_options(;recursive=true, pluto=true, literate=true, smart_filter=true)
-    cd(joinpath(@__DIR__, "..", "docs"))
     pluto_pages = pluto ? build_pluto(DocTools, "pluto_notebooks"; recursive, smart_filter) : String[]
 
     literate_pages = literate ? build_literate(DocTools, "literate_notebooks"; recursive, smart_filter) : String[]
 
     default_makedocs(;
+        root = DOCS_DIR,
         sitename="Doctools.jl",
         modules=[DocTools],
         authors="PlantingSpace",
-        repo="https://github.com/plantingspace/doctools/-/blob/{commit}{path}#{line}",
         macros=Dict(:ps => ["{PlantingSpace}"], :Lc => ["\\mathcal{L}"]),
         notebooks=[pluto_pages; literate_pages],
         pages=["Home" => "index.md"],
     )
 end
 
+
 function rm_docs_build()
     cd(joinpath(@__DIR__, ".."))
-    rm(joinpath(@__DIR__, "..", "docs", "build"); recursive=true)
+    for dir in [BUILD_DIR, SRC_ASSETS_DIR, SRC_NOTEBOOKS_DIR]
+        isdir(dir) && rm(dir; recursive=true)
+    end
 end
 
 rm_docs_build()
@@ -30,12 +38,29 @@ rm_docs_build()
     @testset "Test use of recursion" begin
         try
             build_docs_with_options(recursive=true, smart_filter=false)
-            @test 
+            @test isfile(joinpath(BUILD_DIR, "assets", "notebooks", "Pluto Notebook.html"))
+            @test isfile(joinpath(BUILD_DIR, "assets", "notebooks", "Subfolder Pluto", "Subfolder Pluto Notebook.html"))
+            @test isfile(joinpath(NOTEBOOKS_DIR, "Literate Notebook.html"))
+            @test isfile(joinpath(NOTEBOOKS_DIR, "Pluto Notebook.html"))
+            @test isfile(joinpath(NOTEBOOKS_DIR, "Subfolder Literate", "Subfolder Literate Notebook.html"))
+            @test isfile(joinpath(NOTEBOOKS_DIR, "Subfolder Pluto", "Subfolder Pluto Notebook.html"))
+        catch e
+            rethrow(e)
+        finally
+            rm_docs_build()
+        end
+        try
+            build_docs_with_options(recursive=false, smart_filter=false)
+            @test isfile(joinpath(BUILD_DIR, "assets", "notebooks", "Pluto Notebook.html"))
+            @test !isfile(joinpath(BUILD_DIR, "assets", "notebooks", "Subfolder Pluto", "Subfolder Pluto Notebook.html"))
+            @test isfile(joinpath(NOTEBOOKS_DIR, "Literate Notebook.html"))
+            @test isfile(joinpath(NOTEBOOKS_DIR, "Pluto Notebook.html"))
+            @test !isfile(joinpath(NOTEBOOKS_DIR, "Subfolder Literate", "Subfolder Literate Notebook.html"))
+            @test !isfile(joinpath(NOTEBOOKS_DIR, "Subfolder Pluto", "Subfolder Pluto Notebook.html"))
         catch e
             rethrow(e)
         finally
             rm_docs_build()
         end
     end
-
 end
